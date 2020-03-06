d1eb3484926c912083744be3d7661f6b82ae19cc
borg <antoniovf@al.insper.edu.br>
2020-03-06 17:49:55 -0300

    Retas Corretas

--- Atividades_Robot/AT3-Em_Desenvolvimento/atividade3.py
+++ Atividades_Robot/AT3-Em_Desenvolvimento/atividade3.py
@@ -19,85 +19,30 @@ while True:
 
 #    video_eq = cv2.equalizeHist(gray)
 
-    ret, limiarizada = cv2.threshold(gray, 190, 255, cv2.THRESH_BINARY)
+    ret, limiarizada = cv2.threshold(gray, 210, 255, cv2.THRESH_BINARY)
 
     lines = cv2.HoughLines(limiarizada,1, np.pi/180, 200)
 
     for line in lines:
         for rho,theta in line:
-            a = np.cos(theta)
+            m = np.cos(theta)
             b = np.sin(theta)
-            print(a)
+            
 #            time.sleep(0.5)
-            if a > 0.5 or a < -0.5:
-                x0 = a*rho
+
+            if 0.7 > m > 0.6 or -0.7 > m > -0.8:
+                if m < 0:
+                    print(m)
+                x0 = m*rho
                 y0 = b*rho
                 x1 = int(x0 + 1000*(-b))
-                y1 = int(y0 + 1000*(a))
+                y1 = int(y0 + 1000*(m))
                 x2 = int(x0 - 1000*(-b))
-                y2 = int(y0 - 1000*(a))
+                y2 = int(y0 - 1000*(m))
                 cv2.line(video,(x1,y1),(x2,y2),(0,255,0),10)
             else:
                 pass
 
-#    a,b,c = lines.shape
-#    for i in range(a):
-#        rho1 = lines[0][0][0]
-#        theta1 = lines[0][0][1]
-#        rho2 = lines[1][0][0]
-#        theta2 = lines[1][0][1]
-        
-#        a1 = math.cos(theta1)
-#        b1 = math.sin(theta1)
-#        x0, y0 = a1*rho1, b1*rho1
-#        pt1 = (int(x0+1000*(-b1)), int(y0+1000*(a1)))
-#        pt2 = (int(x0-1000*(-b1)), int(y0-1000*(a1)))
-#        cv2.line(video, pt1, pt2, (0, 0, 255), 3, cv2.LINE_AA)
-
-#        a2 = math.cos(theta2)
-#        b2 = math.sin(theta2)
-#        x0_2, y0_2 = a2*rho2, b2*rho2
-#        pt2_1 = (int(x0_2+1000*(-b2)), int(y0_2+1000*(a2)))
-#        pt2_2 = (int(x0_2-1000*(-b2)), int(y0_2-1000*(a2)))
-#        cv2.line(video, pt2_1, pt2_2, (0, 0, 255), 3, cv2.LINE_AA)
-
-#    try:
-#        line1 = lines[0][0]    
-#        theta1 = line1[1]
-#        rho1 = line1[0]
-#        a1 = np.cos(theta1)
-#        b1 = np.sin(theta1)
-#        x0_1 = a1*rho1
-#        y0_1 = b1*rho1
-#        x1_1 = int(x0_1 + 1000*(-b1))
-#        y1_1 = int(y0_1 + 1000*(a1))
-#        x2_1 = int(x0_1 - 1000*(-b1))
-#        y2_1 = int(y0_1 - 1000*(a1))
-
-#        cv2.line(video, (x1_1,y1_1), (x2_1,y2_1), (0,0,255), 3)
-        
-#    except:
-#        pass
-        
-#    try:
-#        line2 = lines[1][0]
-#        rho2 =  line2[0]
-#        theta2 = line2[1]
-#       a2 = np.cos(theta2)
-#       b2 = np.sin(theta2)
-#        x0_2 = a2*rho2
-#        y0_2 = b2*rho2
-#        x1_2 = int(x0_2 + 1000*(-b2))
-#       y1_2 = int(y0_2 + 1000*(a2))
-#        x2_2 = int(x0_2 - 1000*(-b2))
-#        y2_2 = int(y0_2 - 1000*(a2))
-
-#        cv2.line(video, (x2_1,y2_1), (x2_2,y2_2), (0,255,0), 3)
-        
-#    except:
-#        pass
-        #cv2.line(video,(x1,y1),(x2,y2),(0,0,255),2)
- 
     #lower_white = np.array([0, 0, 150])
     #higher_white = np.array([255, 30, 255])
 
